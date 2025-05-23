497188b9fc3fc0fae7e412c2ca7e0a81f5255d88
Arjun Balu <arjunbear2000@gmail.com>
2025-05-23 06:53:22 +0530

    stuff

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 
-const Search = ({searchTerm, setSearchTerm}) => {
-
+const Search = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className='search'>
         <div>
-            <img src="search.svg" alt="search" />
-
+            <img src="/search.svg" alt="search" />
             <input 
             type="text" 
-            placeholder='Search through thousands of movies' 
+            placeholder='Search for thousands of movies' 
             value={searchTerm} 
             onChange={(e) => setSearchTerm(e.target.value)}
             />
-
         </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,5 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<div>
-<App />
-</div>
-    
-
+    <App />
 )
